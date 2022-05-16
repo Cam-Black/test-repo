f8e6354e5bed59c59e359587d3786d7067220c4e
Cam-Black <cameronablack1998@gmail.com>
2022-05-16 16:01:23 +0100

    Renamed add_products to add_entries

--- my_gameshop/add_entries.sql
+++ my_gameshop/add_entries.sql
@@ -1,6 +1,11 @@
+USE my_gameshop;
+
 INSERT INTO customers(full_name, email)
 	VALUES('John Doe', 'johndoe@email.com');
+INSERT INTO customers (full_name, email) 
+    VALUES('Jane Doe', 'janedoe@email.com');
 INSERT INTO product(product_name, product_price)
 	VALUES('Call of Duty', 40.99);
+
 INSERT INTO orders(fk_customer_id, fk_product_id)
 	VALUES(1, 1);
