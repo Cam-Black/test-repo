f556073bd8375987286facc217fed52cdf4bf00d
Cam-Black <cameronablack1998@gmail.com>
2022-05-16 16:42:37 +0100

    Updated sql scripts with fixes for queries and adding items

--- my_gameshop/add_entries.sql
+++ my_gameshop/add_entries.sql
@@ -4,8 +4,21 @@ INSERT INTO customers(full_name, email)
 	VALUES('John Doe', 'johndoe@email.com');
 INSERT INTO customers (full_name, email) 
     VALUES('Jane Doe', 'janedoe@email.com');
+INSERT INTO customers (full_name, email)
+	VALUES('Gerry Atrics', 'gerryatric@nhs.com');
+
 INSERT INTO product(product_name, product_price)
 	VALUES('Call of Duty', 40.99);
+INSERT INTO product (product_name, product_price) 
+	VALUES('GTA VI', 55.99);
+INSERT INTO product (product_name, product_price) 
+	VALUES('Elder Scrolls 6: Deluxe', 75.99);
+INSERT INTO product (product_name, product_price) 
+	VALUES('The Witcher 4', 45.99);
 
 INSERT INTO orders(fk_customer_id, fk_product_id)
 	VALUES(1, 1);
+INSERT INTO orders (fk_customer_id, fk_product_id) 
+	VALUES(2, 3);
+INSERT INTO orders (fk_customer_id, fk_product_id) 
+	VALUES(3, 2);
--- my_gameshop/query_database.sql
+++ my_gameshop/query_database.sql
@@ -1,6 +1,5 @@
 USE my_gameshop;
 
-
 SELECT * FROM customers;
 SELECT * FROM product;
 SELECT * FROM orders;
