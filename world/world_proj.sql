ec194764bceb87cd1c2d455646ddbc9ba1441e1b
Cam-Black <cameronablack1998@gmail.com>
2022-05-17 10:44:26 +0100

    Added query for Life Expectancy and Population from Argentina

--- world/world_proj.sql
+++ world/world_proj.sql
@@ -1,3 +1,8 @@
 USE world;
 
 SELECT COUNT(CountryCode) AS NumberOfUSCities FROM city WHERE CountryCode='USA';
+
+SELECT * FROM country;
+
+SELECT Name, LifeExpectancy, Population FROM country WHERE Code = 'ARG';
+
