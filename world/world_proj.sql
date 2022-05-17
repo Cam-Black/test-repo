7d15597c7aac62851e9569ede574c49baded550e
Cam-Black <cameronablack1998@gmail.com>
2022-05-17 10:56:14 +0100

    Found out the country with the highest life expectancy, added comments to describe queries

--- world/world_proj.sql
+++ world/world_proj.sql
@@ -1,8 +1,15 @@
 USE world;
 
+-- Using COUNT, get the number of cities in the USA.
 SELECT COUNT(CountryCode) AS NumberOfUSCities FROM city WHERE CountryCode='USA';
 
+-- See all fields in Country table
 SELECT * FROM country;
 
+-- Find out the population and life expectancy for people in Argentina.
 SELECT Name, LifeExpectancy, Population FROM country WHERE Code = 'ARG';
 
+-- Using IS NOT NULL, ORDER BY, and LIMIT, which country has the highest life expectancy?
+SELECT Name, LifeExpectancy FROM country WHERE LifeExpectancy IS NOT NULL  ORDER BY LifeExpectancy DESC LIMIT 1;
+
+-- Using JOIN ... ON, find the capital city of Spain.
