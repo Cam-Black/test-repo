aabe1fd8ac616b25ce7cc496e8f3b7c2ca1a86e8
Cam-Black <cameronablack1998@gmail.com>
2022-05-18 15:13:33 +0100

    Added database to use

--- movielens/movielens_proj.sql
+++ movielens/movielens_proj.sql
@@ -1,3 +1,5 @@
+USE movielens;
+
 -- List the titles and release dates of movies released between 1983-1993 in reverse chronological order.
 -- Without using LIMIT, list the titles of the movies with the lowest average rating.
 -- List the unique records for Sci-Fi movies where male 24-year-old students have given 5-star ratings.
