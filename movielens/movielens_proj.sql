92ea01d08831017115d77f4e319834953934f1de
Cam-Black <cameronablack1998@gmail.com>
2022-05-19 10:11:50 +0100

    Updated sql scripts to finish a couple queries that were not fully complete, and some comment formatting

--- movielens/movielens_proj.sql
+++ movielens/movielens_proj.sql
@@ -7,20 +7,31 @@ WHERE release_date BETWEEN '1983-01-01' AND '1993-01-01'
 ORDER BY release_date ASC;
 
 -- Without using LIMIT, list the titles of the movies with the lowest average rating.
-SELECT m.title AS Movie, r.rating AS Rating
+SELECT m.title AS Movie, AVG(r.rating) AS Average_Rating
 FROM movies m
 INNER JOIN ratings r ON m.id = r.movie_id
-ORDER BY r.rating ASC;
+GROUP BY m.title
+HAVING Average_Rating = (
+	SELECT MIN(Average_Rating)
+	FROM (
+		SELECT AVG(r.rating) AS Average_Rating
+        FROM ratings r
+        GROUP BY movie_id
+    ) AS Table1
+)
+ORDER BY AVG(r.rating) ASC;
 
 -- List the unique records for Sci-Fi movies where male 24-year-old students have given 5-star ratings.
-SELECT m.id, m.title AS Movie
+SELECT m.title AS Movie
 FROM movies m
 INNER JOIN genres_movies gm ON m.id = gm.movie_id 
 INNER JOIN genres g ON g.id = gm.genre_id
 INNER JOIN ratings r ON r.movie_id = m.id
 INNER JOIN users u ON r.user_id = u.id
 INNER JOIN occupations o ON u.occupation_id = o.id
-WHERE g.name = 'sci-fi' AND u.gender = 'M' AND u.age = 24 AND o.name = 'Student' AND r.rating = 5;
+WHERE g.name = 'sci-fi' AND u.gender = 'M' AND u.age = 24 AND o.name = 'Student' AND r.rating = 5
+GROUP BY title
+ORDER BY title;
 
 -- List the unique titles of each of the movies released on the most popular release day.
 SELECT title AS Movie, release_date AS ReleaseDate
