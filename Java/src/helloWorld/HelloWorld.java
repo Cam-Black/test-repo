d855ac1da4496585d4b1e0b6d967a3ed4787f063
Cam-Black <cameronablack1998@gmail.com>
2022-05-19 16:34:45 +0100

    Added java folder containing HelloWorld project

---
+++ Java/src/helloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+package helloWorld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		String message = "Hello there, i am";
+		int number = 40;
+		System.out.println(String.format(message + ' ' + number));
+	}
+}
